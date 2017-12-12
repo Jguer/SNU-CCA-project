9dc268b40c1b9de63d26ae416dc6774f3eb0bbdc
Jguer <me@jguer.space>
2017-12-12 14:36:39 +0900

    New PCA

--- pca.R
+++ pca.R
@@ -1,25 +1,28 @@
 # Including library
-library(rpart)
-library(rpart.plot)
-library(randomForest)
+#library(rpart)
+#library(rpart.plot)
+#library(randomForest)
+library(FactoMineR)
 
-# Import Data
 voice_data <- read.csv("voice.csv", header=TRUE, sep=";")
 names(voice_data) <- c("id", "meanfreq", "sd", "median", "Q25", "Q75", "IQR", "skew",
                  "kurt", "sp.ent", "sfm", "mode", "centroid", "meanfun",
                  "minfun", "maxfun", "meandom", "mindom", "maxdom", "dfrange",
                  "modindx", "label")
-voice_data$label <- as.factor(voice_data$label)
+#voice_data$label <- as.factor(voice_data$label)
 
 # Information about Dataset
 summary(voice_data)
 dim(voice_data)
 
+
+result <- PCA(voice_data[,2:21]) # graphs generated automatically
+# Import Data
 # Prepare PCA
-label_col <- which(colnames(voice_data) == "label")
+#label_col <- which(colnames(voice_data) == "label")
 
 # Perform PCA
-voice_pca <- prcomp(voice_data[,2:21])
-summary(voice_pca)
-biplot(voice_pca, col = c("red", "blue"), scale = 0)
+#voice_pca <- prcomp(voice_data[,2:21])
+#summary(voice_pca)
+#biplot(voice_pca, col = c("red", "blue"), scale = 0)
 
